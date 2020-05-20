ed826fcf4ba3ff97bded7984f6600c81178d5726
havvarslan <havva_arslan@yahoo.com>
2020-05-20 18:48:29 +0100

    add TestBranch.java

--- src/test/java/tests/TestBranch.java
+++ src/test/java/tests/TestBranch.java
@@ -1,8 +1,7 @@
 package tests;
 
-public class testexam {
+public class TestBranch {
     public static void main(String[] args) {
-
-        System.out.println("hello");
+        System.out.println("Hi everyone");
     }
 }
