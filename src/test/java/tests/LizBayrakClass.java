9cc16e1b416c933f9690661906d51b0c14f92234
azizshn <azizsahin@yandex.com>
2020-05-23 11:28:57 +0100

    update

--- src/test/java/tests/LizBayrakClass.java
+++
@@ -1,8 +0,0 @@
-package tests;
-
-public class LizBayrakClass {
-
-    public static void main(String[] args) {
-        System.out.println("This is Liz's class");
-    }
-}
--- src/test/java/tests/elif.java
+++ src/test/java/tests/elif.java
@@ -5,4 +5,7 @@ public class elif {
         System.out.println("hello everyone");
     }
 
+
+//asdasdasdasd
+
 }
