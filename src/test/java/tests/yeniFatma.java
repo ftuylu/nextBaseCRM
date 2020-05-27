a56605d653dcf68e4a8c12f2ba6c3b69bd70a8a3
ftuylu <ftavus@yahoo.com>
2020-05-27 22:21:15 +0100

    add userStory.Java

--- src/test/java/tests/yeniFatma.java
+++
@@ -1,9 +0,0 @@
-package tests;
-
-public class yeniFatma {
-
-    public static void main(String[] args) {
-        System.out.printf("hello new github");
-        System.out.println("selam");
-    }
-}
