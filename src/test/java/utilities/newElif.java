e4a62871119af79528fb925999645de29df6a052
elialan <MyLovelySoap@yahoo.com>
2020-05-28 09:45:53 +0100

    add newElif

---
+++ src/test/java/utilities/newElif.java
@@ -0,0 +1,8 @@
+package utilities;
+
+public class newElif {
+    public static void main(String[] args) {
+        System.out.println("just trying");
+        System.out.println("28/05/2020");
+    }
+}
