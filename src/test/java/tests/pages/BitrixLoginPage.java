872d1efd1996b3d4b4a6ba0f3ded10f701930d7a
azizshn <azizsahin@yandex.com>
2020-05-28 11:26:55 +0100

    add

---
+++ src/test/java/tests/pages/BitrixLoginPage.java
@@ -0,0 +1,35 @@
+package tests.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+
+public class BitrixLoginPage {
+
+
+    public BitrixLoginPage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+
+    @FindBy(name = "USER_LOGIN")
+    public WebElement usernameInput;
+
+    @FindBy(xpath = "//input[@type='password']")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//input[@type='submit']")
+    public WebElement loginBtn;
+
+
+    public void login(String usernameStr, String passwordStr) {
+
+        usernameInput.sendKeys(usernameStr);
+        passwordInput.sendKeys(passwordStr);
+        loginBtn.click();
+    }
+
+
+}
---
+++ src/test/java/tests/userStory4.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class userStory4 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+
+}
