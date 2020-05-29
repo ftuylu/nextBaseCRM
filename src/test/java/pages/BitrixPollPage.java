989e8e53c5735030b15d575d39dbd800a7552dcc
ftuylu <ftavus@yahoo.com>
2020-05-29 21:41:12 +0100

    BitrixPollPage.Java

---
+++ src/test/java/pages/BitrixPollPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class BitrixPollPage extends BitrixBasePage{
+
+    public void BitrixPollPage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+
+}
--- src/test/java/tests/user_story_4/userStory4.java
+++ src/test/java/tests/user_story_4/userStory4.java
@@ -1,6 +1,7 @@
 package tests.user_story_4;
 
 import org.testng.annotations.Test;
+import pages.BitrixActStrPage;
 import pages.BitrixLoginPage;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -15,6 +16,8 @@ public class userStory4 extends TestBase {
       extentLogger = report.createTest("Bitrix24 User Story4 AC1");
       BitrixLoginPage loginPage =new BitrixLoginPage();
       loginPage.loginHelpdesk();
+      BitrixActStrPage pollBtn =new BitrixActStrPage();
+      pollBtn.poll.click();
 
    }
 }
