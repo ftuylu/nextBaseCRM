8927f9708281d97c7b821ef96ee7125294ca0135
mstfyt <mstfyt@gmail.com>
2020-05-29 18:33:59 +0100

    update BitrixActStrPage.java

--- src/test/java/pages/BitrixActStrPage.java
+++ src/test/java/pages/BitrixActStrPage.java
@@ -1,8 +1,10 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utilities.BrowserUtils;
 import utilities.Driver;
 
 public class BitrixActStrPage extends BitrixBasePage{
@@ -11,8 +13,16 @@ public class BitrixActStrPage extends BitrixBasePage{
         PageFactory.initElements(Driver.get(),this);
     }
 
-    @FindBy(id="feed-add-post-form-tab-vote")
-    public WebElement poll;
+
+    public void navigateActiveStream(String module, String tab){
+
+        String moduleLocator = "//a[@title='" + module + "']";
+        Driver.get().findElement(By.xpath(moduleLocator)).click();
+        BrowserUtils.waitForVisibility(By.xpath(moduleLocator),3);
+
+        String tabLocator= "(//span[.='"+tab+"'])[1]";
+        Driver.get().findElement(By.xpath(tabLocator)).click();
+    }
 
 
 
