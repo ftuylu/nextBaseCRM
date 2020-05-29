92104ee2bd93b11022ad256f518bc55a638d82ba
ftuylu <ftavus@yahoo.com>
2020-05-29 14:08:59 +0100

    update BitrixActStrPage.Java

--- src/test/java/pages/BitrixActStrPage.java
+++ src/test/java/pages/BitrixActStrPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -7,6 +9,14 @@ public class BitrixActStrPage extends BitrixBasePage{
 
     public BitrixActStrPage( ){ PageFactory.initElements(Driver.get(),this); }
 
+    @FindBy(id="feed-add-post-form-tab-vote")
+    public WebElement poll;
+    @FindBy (id ="feed-add-post-form-tab-message")
+    public WebElement Message;
+    @FindBy (id ="feed-add-post-form-tab-calendar")
+    public WebElement Event;
+    @FindBy (id ="feed-add-post-form-tab-tasks")
+    public WebElement Task;
 
 
 }
