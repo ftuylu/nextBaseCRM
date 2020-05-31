862093c97a189bc34f54ce7f7d6c98419ce17692
ftuylu <ftavus@yahoo.com>
2020-05-31 14:29:33 +0100

    update userstory4.Java

--- src/test/java/tests/user_story_4/userStory4.java
+++ src/test/java/tests/user_story_4/userStory4.java
@@ -1,17 +1,45 @@
 package tests.user_story_4;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.BitrixActStrPage;
 import pages.BitrixLoginPage;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import pages.BitrixPollPage;
 import tests.TestBase;
+import utilities.BrowserUtils;
+import utilities.Driver;
 
 public class userStory4 extends TestBase {
 
    @Test
-   public void pollTabClickUpload(){
-      extentLogger = report.createTest("Bitrix24 User Story4 AC1");
+   public void pollTabAddMention(){
+      extentLogger = report.createTest("Bitrix24 User Story4 AC6");
       BitrixLoginPage loginPage =new BitrixLoginPage();
       loginPage.loginHelpdesk();
+      extentLogger.info("Login successfully");
+      BitrixActStrPage pollBtn =new BitrixActStrPage();
+      pollBtn.poll.click();
+      extentLogger.info("click Poll button");
+      BitrixPollPage pollPage =new BitrixPollPage();
+      BrowserUtils.waitFor(2);
+      pollPage.addMention.click();
+      extentLogger.info("click addMention button");
+      String choosen=pollPage.selectContact.getText();
+      pollPage.selectContact.click();
+      extentLogger.info("select a contact");
+      Assert.assertEquals(pollPage.text2.getText(),choosen,"verify add mention");
+      extentLogger.pass("Pass");
 
    }
 }
+
+
      
