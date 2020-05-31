17e747c3750498b9c0f7f669b391a68bcbe47a2f
lizbayrak <liz.bayrak@yahoo.co.uk>
2020-05-31 21:09:17 +0100

    Update

--- src/test/java/pages/BitrixPollPage.java
+++ src/test/java/pages/BitrixPollPage.java
@@ -7,12 +7,30 @@ import utilities.Driver;
 
 public class BitrixPollPage extends BitrixBasePage{
 
-    public void BitrixPollPage(){
+    public BitrixPollPage(){
         PageFactory.initElements(Driver.get(),this);
     }
     @FindBy(xpath = "(//span[@title='Add mention'])[1]")
     public WebElement addMention;
     @FindBy(xpath = "//div[@class='bx-finder-box-item-t7-name']")
     public  WebElement selectContact;
+
+    @FindBy(xpath = "(//span[@title='Upload files'])[1]")
+    public WebElement uploadFileButton;
+
+    @FindBy(id = "feed-add-post-destination-input-box")
+    public WebElement toPeopleBox;
+
+    @FindBy(xpath = "(//span[@title='Link'])[1]")
+    public WebElement linkIcon;
+
+    @FindBy(xpath = "(//span[@title='Insert video'])")
+    public WebElement videoIcon;
+
+    @FindBy(xpath = "(//span[@title='Quote text'])")
+    public WebElement insertQuotebutton;
+
+
+
 
 }
--- src/test/java/tests/user_story_4/userStory4Liz.java
+++ src/test/java/tests/user_story_4/userStory4Liz.java
@@ -1,10 +1,14 @@
 package tests.user_story_4;
 
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 import pages.BitrixActStrPage;
 import pages.BitrixLoginPage;
+import pages.BitrixPollPage;
 import pages.DashboardPage;
 import tests.TestBase;
+import utilities.BrowserUtils;
+import utilities.Driver;
 
 public class userStory4Liz extends TestBase {
 
@@ -20,6 +24,21 @@ public class userStory4Liz extends TestBase {
 
         activityStream.poll.click();
 
+    }
+
+    @Test
+    public void AC1UploadFiles() {
+
+        BitrixLoginPage bitrixLoginPage = new BitrixLoginPage();
+        bitrixLoginPage.loginHelpdesk();
+
+        new DashboardPage().navigateToModule("Activity Stream");
+
+        BitrixActStrPage activityStream = new BitrixActStrPage();
+        activityStream.poll.click();
+
+        BitrixPollPage bitrixPollPage = new BitrixPollPage();
+        BrowserUtils.waitForVisibility(bitrixPollPage.uploadFileButton,5).click();
 
 
 
@@ -27,7 +46,10 @@ public class userStory4Liz extends TestBase {
 
 
 
+
+
+        }
     }
 
 
-}
+
