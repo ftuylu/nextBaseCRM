3ae330f2ad5991ce0e3140c43579bfcae7f0fb03
Havva <47014284+havvarslan@users.noreply.github.com>
2020-06-02 02:38:27 +0100

    Update MorePage

--- src/test/java/pages/MorePage.java
+++ src/test/java/pages/MorePage.java
@@ -36,72 +36,17 @@ public class MorePage extends BitrixBasePage {
     @FindBy(id="bx-destination-tag")
     public WebElement addRecipent;
 
-//    @FindBy(css = ".bx-finder-box-tab bx-lm-tab-department")
-//    public WebElement employeeaAndDepartment;
-
     @FindBy(xpath = "//div[text() = 'marketing15@cybertekschool.com']")
     public WebElement emailFromEmployee;
 
     @FindBy(css = ".feed-add-post-del-but")
     public WebElement deleteAllEmployee;
 
-<<<<<<< HEAD
     @FindBy(xpath = "//*[@id='bx-b-link-blogPostForm']/span")
     public WebElement link;
 
     @FindBy(id = "linkidPostFormLHE_blogPostForm-text")
     public WebElement linkText;
-
-
-
-
-
-=======
-    // =====+=+++=+=+====++=+=+==+=+==+=+=+=++=+=+=++=+=+==
-    // UPLOAD FINDERS +=====++===+===+=++=+=+=++=+=+=+=+=+=
-    @FindBy(xpath = "(//*[.='Upload files and images'])[1]")
-    public WebElement uploadFileOrImage;
-    // EDITOR TEXT BAR FINDERS ====+=++=+=+=++=+=+=++++=+=+=+=
-    @FindBy(id = "bx-html-editor-tlbr-idPostFormLHE_blogPostForm")
-    public WebElement editorTextBar;
-    @FindBy(xpath = "//*[@Title='Visual editor']")
-    public WebElement visualEditorButton;
-    //  TOPIC TEXT FINDERS ======++=+=++=+=+=++++=+=++=+=+=+=++===+
-    @FindBy(xpath = "//*[@title='Topic']")
-    public WebElement topicTextButton;
-    @FindBy(css = ".feed-add-post-inp.feed-add-post-inp-active")
-    public WebElement topicTextBox;
-    // VIDEO RECORDER FINDERS ===+=+=+==+=+=++=+++=++=+=+=++=+=+=++=+
-    @FindBy(xpath = "(//span[.='Record Video'])[1]")
-    public WebElement videoRecordButton;
-    @FindBy(xpath = "(//*[@class='popup-window-content'])[4]")
-    public WebElement videoRecordPopUp;
-    @FindBy(css = ".popup-window-button.popup-window-button-blue")
-    public WebElement videoSettingAllowButton;
-    // ADD TAG FINDERS =====+==+=++=++=+=+=+++=+=++=+=+==+=+=++=+=+=
-    @FindBy(id = "bx-b-tag-input-blogPostForm")
-    public WebElement addTagButton;
-    @FindBy(id = "TAGS_blogPostForm67abSn")
-    public WebElement addTagBox;
-    @FindBy(id = "TAGS_blogPostForm67abSn_div")
-    public List<WebElement> addTagExisting;
-    @FindBy(css = ".search-popup-el.search-popup-el-name")
-    public List<WebElement> addTagSelected;
-    @FindBy(css = ".search-popup-el.search-popup-el-name")
-    public WebElement addTagFirstExisting;
-    @FindBy(xpath = "(//*[text()='Add'])[2]")
-    public WebElement addButton;
-    @FindBy(css = ".feed-add-post-tags-wrap")
-    public WebElement addSelectedTagContainer;
-    // EMOJI FINDERS ===+===+=++=+=+==+=+==+=+=+==+=+=++=+=+==
-    @FindBy(css = ".feed-add-grat-box")
-    public List<WebElement> emojiList;
-    @FindBy(css = ".feed-add-grat-box")
-    public WebElement emojiButton;
-    // =====++===+==+++==+=++++=+=++=+=+=++=+=+=++++=+=+=++=+=+=
->>>>>>> 35e1f8c79dda46ab8ffafc5bae7d5c47ff5e965d
-
-
 
     public void navigateInMore(String module) {
         String tabLocator = "//span[@id='feed-add-post-form-link-text']";
