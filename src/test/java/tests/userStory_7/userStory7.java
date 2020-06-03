bf140996a1c20e538e1f8be5c93c4da9dc2395d9
havvarslan <havva_arslan@yahoo.com>
2020-06-03 02:29:11 +0100

    update in userStory_7

--- src/test/java/tests/userStory_7/userStory7.java
+++ src/test/java/tests/userStory_7/userStory7.java
@@ -1,17 +1,20 @@
 package tests.userStory_7;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.BitrixLoginPage;
-import pages.MorePage;
-import pages.TaskPageForUserStory2;
+import pages.*;
 import tests.TestBase;
 import utilities.BrowserUtils;
+import utilities.Driver;
 
 
 public class userStory7 extends TestBase {
 
+    //#AC_1
     @Test
-
     public void uploadFiles() {
         BitrixLoginPage loginPage = new BitrixLoginPage();
         loginPage.loginMarketing();
@@ -36,10 +39,10 @@ public class userStory7 extends TestBase {
 
     }
 
+    //#AC_2
     @Test
     public void selectDocumentFromBitrix() {
-        BitrixLoginPage loginPage = new BitrixLoginPage();
-        loginPage.loginMarketing();
+        new BitrixLoginPage().loginMarketing();
 
         MorePage morePage = new MorePage();
         morePage.navigateInMore("Announcement");
@@ -54,11 +57,9 @@ public class userStory7 extends TestBase {
         employeeAndDepartment.employeeAndDepartments.click();
         employeeAndDepartment.userMail.click();
 
-
         BrowserUtils.waitFor(3);
         morePage.uploadFiles.click();
         BrowserUtils.waitFor(3);
-
 
         morePage.selectDocumentFromBitrix.click();
         BrowserUtils.clickWithJS(morePage.myDrive);
@@ -66,16 +67,130 @@ public class userStory7 extends TestBase {
 
         morePage.sendButton.click();
 
+    }
 
-        //attach link
-        BrowserUtils.waitFor(3);
+    //#AC_3
+    @Test
+    public void attachLink(){
+        new BitrixLoginPage().loginHr();
+
+        MorePage morePage = new MorePage();
         morePage.navigateInMore("Announcement");
         morePage.link.click();
 
+        BitrixAppreciationPage appreciationPage = new BitrixAppreciationPage();
+        appreciationPage.addUrlToLink("Google","www.google.com");
+        appreciationPage.saveLinkButton.click();
+        BrowserUtils.waitFor(3);
+        morePage.sendButton.click();
 
     }
 
+    //#AC_4
+    @Test
+    public void insertVideo(){
+        new BitrixLoginPage().loginHr();
 
+        MorePage morePage = new MorePage();
+        morePage.navigateInMore("Announcement");
+
+        BitrixPollPage pollPage = new BitrixPollPage();
+        pollPage.videoIcon.click();
+
+        String videoUrl ="https://www.youtube.com/watch?v=IVsLfuWISlM";
+
+        BrowserUtils.waitForVisibility(pollPage.videoUrlBox, 5).sendKeys(videoUrl);
+        BrowserUtils.waitFor(5);
+        pollPage.linkTextBoxSaveButton.click();
+        BrowserUtils.waitFor(3);
+
+        morePage.sendButton.click();
+    }
+
+    //#AC_5
+    @Test
+    public void quote(){
+        new BitrixLoginPage().loginHr();
+
+        MorePage morePage = new MorePage();
+
+        morePage.navigateInMore("Announcement");
+
+        BitrixAppreciationPage appreciationPage = new BitrixAppreciationPage();
+
+        BrowserUtils.waitForVisibility(appreciationPage.commaIcon,5).click();
+        BrowserUtils.waitFor(3);
+        driver.switchTo().frame(driver.findElement(By.xpath("//form//iframe")));
+        appreciationPage.quoteTextBox.sendKeys("Practice makes perfect");
+        BrowserUtils.waitFor(3);
+        driver.switchTo().parentFrame();
+        morePage.sendButton.click();
+
+    }
+
+    //AC_6
+    @Test
+    public void addMention(){
+        new BitrixLoginPage().loginHr();
+
+        BitrixPollPage pollPage = new BitrixPollPage();
+        pollPage.addMention.click();
+        pollPage.selectContact.click();
+        String selected = pollPage.selectContact.getText();
+        Assert.assertEquals(pollPage.text2.getText(), selected, "verify add mention");
+    }
+
+    //AC_7
+    @Test
+    public void VisualEditor(){
+        new BitrixLoginPage().loginHr();
+
+        MorePage morePage = new MorePage();
+        morePage.navigateInMore("Announcement");
+        morePage.visualEditorButton.click();
+
+        Assert.assertTrue(morePage.editorTextBar.isDisplayed(), "Verify that editor text bar is displayed");
+    }
+
+    //#AC_8
+    @Test
+    public void topicIsDisplayed(){
+        new BitrixLoginPage().loginHr();
+
+        MorePage morePage = new MorePage();
+        morePage.navigateInMore("Announcement");
+        morePage.topicTextButton.click();
+
+        Assert.assertTrue(morePage.topicTextBox.isDisplayed(),"Verify that topic text box is displayed");
+
+    }
+
+    //#AC_9
+    @Test
+    public void recordVideo(){
+        System.out.println("There is bug here");
+    }
+
+    //#AC_10
+    @Test
+    public void tags(){
+        new BitrixLoginPage().loginHr();
+        MorePage morePage = new MorePage();
+        morePage.navigateInMore("Announcement");
+
+        morePage.addTagButton.click();
+        morePage.addTagBox.sendKeys("selale" + Keys.ENTER);
+
+        morePage.addTagButton.click();
+        morePage.addTagBox.sendKeys("a");
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) Driver.get();
+        javascriptExecutor.executeScript("arguments[0].click()", morePage.addTagFirstExisting);
+        javascriptExecutor.executeScript("arguments[0].click()", morePage.addButton);
+
+        //  Assert.assertTrue(morePage.addSelectedTagContainer.getText().contains("CyberteckSchool"), "verify that cybertekschool added");
+        // Assert.assertTrue(morePage.addSelectedTagContainer.getText().contains("Advice"), "verify that existing Advice tag is selected");
+
+    }
 }
 
 
